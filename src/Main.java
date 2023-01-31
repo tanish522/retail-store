76cf0ff6a938db1e02e039daf5cb98941437db04
Neel <neel02.shah@gmail.com>
2023-01-31 15:24:08 +0530

    Initial commit by Neel

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,39 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         // testing
         System.out.println("Hello world!");
         Transaction t = new Transaction();
-        Product p = new Product();
+//        Product p= new Product();
         User u = new User();
         Catalog c = new Catalog();
-        p.productID = 1;
-        p.qty = 10;
-        p.price = 100;
+        Product book = new Product(1,"Book",200,4);
+        Product bag = new Product(2,"Bag",400,1);
+        Product pen = new Product(3,"Pen",25,10);
+        Product bottle = new Product(4,"Bottle",25,2);
+
+//        p.productID = 1;
+//        p.qty = 10;
+//        p.price = 100;
 //        part2:
 //        {
 //            System.out.println("transaction label where balance is insufficient");
 //        }
-        t.purchase(p,u);
-        // display catalog
+        //t.purchase(book,u);
+        // set catalog
+        c.setProductList(book);
+        c.setProductList(bag);
+        c.setProductList(pen);
+        c.setProductList(bottle);
+
+        //display catalogue
+        c.getCatalog();
+
+        //display products
+        //book.display();
+
+
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -4,6 +4,22 @@ public class Product {
     int productID, price, qty;
     String productName;
 
+    Product(int productID,String productName, int price, int qty)
+    {
+        this.productID=productID;
+        this.price=price;
+        this.qty=qty;
+        this.productName=productName;
+
+    }
+    void display()
+    {
+        System.out.println("Product ID: "+ productID);
+        System.out.println("Product Name: "+ productName);
+        System.out.println("Product Price: "+ price);
+        System.out.println("Product Quantity: "+ qty);
+    }
+
 }
 
 class Catalog {
@@ -11,8 +27,21 @@ class Catalog {
 
     // make display fun of catalog and access using its object
 
-    void displayCatalog(){
 
+    void getCatalog()
+    {
+        for(int i=0;i<productList.size();i++)
+        {
+            System.out.print(productList.get(i).productID + "\t");
+            System.out.print(productList.get(i).productName + "\t");
+            System.out.print(productList.get(i).price + "\t");
+            System.out.print(productList.get(i).qty + "\n");
+        }
+
+    }
+
+    public void setProductList(Product product) {
+        productList.add(product);
     }
 }
 
