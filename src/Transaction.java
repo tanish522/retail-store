479628365c52eba9707229f73f4700c07e87444a
tanish522 <tanishpatel0502@gmail.com>
2023-01-31 11:39:13 +0530

    initial commit

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,6 +2,7 @@ public class Transaction {
     int amount;
     public void purchase(Product p){  // product obj  // user obj
 
+
     }
     public void  msg(){
         System.out.println("transcation method");
