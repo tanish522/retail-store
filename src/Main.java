8889f3c8470d29a56e5a68bb7b13c236d936bec2
jinal <jinalthakor27@gmail.com>
2023-02-01 15:31:04 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,34 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        //part 1 testing 
+        ArrayList<User> userList = new ArrayList<>();
+        User temp = new User();
+        temp.uId = 11;
+        temp.name = "jinal";
+        temp.balance = 0;
+        temp.password = "j";
+        userList.add(temp);
+        
+        temp.loginUser("jinal","j@j");
         // testing
-        System.out.println("Hello world!");
-        Transaction t = new Transaction();
-        Product p = new Product();
-        User u = new User();
-        Catalog c = new Catalog();
-        p.productID = 1;
-        p.qty = 10;
-        p.price = 100;
+        // System.out.println("Hello world!");
+        // Transaction t = new Transacti   on();
+        // Product p = new Product();
+        // User u = new User();
+        // Catalog c = new Catalog();
+        // p.productID = 1;
+        // p.qty = 10;
+        // p.price = 100;
 //        part2:
 //        {
 //            System.out.println("transaction label where balance is insufficient");
 //        }
-        t.purchase(p,u);
+      //  t.purchase(p,u);
         // display catalog
+
+
     }
 }
