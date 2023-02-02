cd642fac6a9aa43ffc9397d9794451cff95b98f1
pratish99 <sonipratish32@gmail.com>
2023-02-02 16:59:23 +0530

    Part 1 Completed with forget password error

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,60 @@
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 import java.util.Scanner;
 public class Main {
 
+    static ArrayList<User> userList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int ch;
+        int ch = 0;
         do {
+
             System.out.println("1. Login \n2. Register \n3. Change Password\n4. Exit");
-            ch = sc.nextInt();
+
+                String temp = sc.next();
+                ch = Integer.parseInt(temp);
+
             switch (ch) {
                 case 1: {
-                    String userName = sc.nextLine();
-                    sc.nextLine();
-                    String password = sc.nextLine();
-                    sc.nextLine();
+                    System.out.println("Enter UserName: ");
+                    String userName = sc.next();
+                    System.out.println("Enter Password: ");
+                    String password = sc.next();
                     User userObj = new User();
-                    if (userObj.loginUser(userName, password)) {
+                    if (userObj.loginUser(userList, userName, password)) {
                         //Go to part 2
                     } else {
                         continue;
                     }
-
+                    break;
                 }
                 case 2: {
-                    User userObj = new User();
-                   // userObj.register();
+                     User userObj = new User();
+                     userObj.register();
+                     userList.add(userObj);
+                     break;
                 }
 
                 case 3: {
+
                     System.out.println("Enter Username to change Password ");
                     String userName = sc.next();
                     User userObj = new User();
-                    userObj.changePassword(userName);
+                    int index = userObj.changePassword(userList, userName);
+                    System.out.println("From Main" + userList.get(index).uId + userList.get(index).password +  userList.get(index).name + userList.get(index).balance);
+                    break;
+                }
+                case 4: {
+                    System.out.println("Thank You For Shopping With Us! ");
+                    break;
                 }
                 default: {
                     System.out.println("Invalid Choice");
+                    break;
                 }
             }
         } while (ch != 4);
--- src/User.java
+++ src/User.java
@@ -3,53 +3,57 @@ import java.util.Scanner;
 
 public class User {
 
-    int uId=0;
-    String name ="";
+    int uId;
+    String name;
     String password;
-    int balance = 0;
+    int balance;
     ArrayList<PurchaseDetail> purchaseList = new ArrayList<PurchaseDetail>();
 
+    public void register(){
+        //users.contains(this.uId);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter UID: ");
+        this.uId = sc.nextInt();
+        System.out.println("Enter Name: ");
+        this.name = sc.next();
+        System.out.println("Enter Password: ");
+        this.password = sc.next();
+        System.out.println("Enter Balance: ");
+        this.balance = sc.nextInt();
+        return;
+    }
 
 
-
-    public boolean loginUser(String userName, String password){
-        for(int i=0;i<users.size();i++){
-            User temp = users.get(i);
+    public boolean loginUser(ArrayList<User> userList, String userName, String password){
+        for(int i=0;i<userList.size();i++){
+            User temp = userList.get(i);
 
             if(temp.name.equals(userName) && temp.password.equals(password)){
-                System.out.print("Welcome "+userName);
+                System.out.println("Welcome "+userName);
                 return true;
             }
         }
         System.out.println("Username Or Password incorrect");
         return false;
     }
-    public boolean changePassword(String userName){
-        for(int i=0;i<users.size();i++){
-            register temp = users.get(i);
+    public int changePassword(ArrayList<User> userList, String userName){
+        for(int i=0;i<userList.size();i++){
+            User temp = userList.get(i);
             if(temp.name.equals(userName)){
                 Scanner sc = new Scanner(System.in);
                 System.out.println("Enter New Password: ");
                 temp.password = sc.next();
-                users.set(i,temp);
+                userList.set(i,temp);
                 System.out.println("Password changed successfully ");
                 sc.close();
-                return true;
+                System.out.println(temp.uId + temp.password +  temp.name + temp.balance);
+                return i;
             }
         }
         System.out.println("Username Does not exists ");
-        return false;
+        return -1;
     }
 }
-class AllUsers{
-    ArrayList<User>  userList = new ArrayList<>();
-
-    void addUser(User u){
-        userList.add(u);
-    }
-
-}
-
 
 class PurchaseDetail {
     String pid;
