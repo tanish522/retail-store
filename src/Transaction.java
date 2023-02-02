d2880e8709a0342b7becdcd279ad5a9f1250858e
pratish99 <sonipratish32@gmail.com>
2023-02-02 23:40:11 +0530

    Part 1 Completed v1.3

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,14 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Transaction {
-<<<<<<< HEAD
-    int totalAmount, pID, purchasedQty;
-    Transaction purchase(Product p, User u) {
-=======
+
     int totalAmount, pID, updatedBal;
     public void purchase(Product p, User u, ArrayList<Product> productList) {
         System.out.println("Your Balance: " + u.balance);
->>>>>>> c17533bcdd6f8dbc2489adc93da2595fedeea376
+
         Scanner sc = new Scanner(System.in);
         Transaction t = new Transaction();
         System.out.println("Enter required quantity : ");
@@ -20,13 +17,9 @@ public class Transaction {
         }
         else{
             u.balance -= totalAmount;
+            //u.purchaseList.add(p);
             p.qty -= purchasedQty;
             System.out.println("Successful transaction");
-<<<<<<< HEAD
-            u.balance-=totalAmount;
-            return t;
-=======
->>>>>>> c17533bcdd6f8dbc2489adc93da2595fedeea376
         }
     }
 }
