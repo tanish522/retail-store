940ebae528aaea15e4f421eb284ac59a20866584
tanish522 <tanishpatel0502@gmail.com>
2023-02-03 11:03:25 +0530

    transaction quantity functionalty added v1

--- src/Transaction.java
+++ src/Transaction.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 public class Transaction {
     int totalAmount, pID, updatedBal;
     public void purchase(Product p, User u, ArrayList<Product> productList, Scanner sc) {
+        int purchasedQty;
         System.out.println("Your Balance: " + u.balance);
-        System.out.println("Enter quantity : ");
-        int purchasedQty = sc.nextInt();
+        do {
+            System.out.println("Enter quantity : ");
+            purchasedQty = sc.nextInt();
+            if(purchasedQty > p.qty)
+                System.out.println("Quantity not available. Enter correct quantity. ");
+        }while (purchasedQty > p.qty);
+
         totalAmount = purchasedQty * p.price;
         if (u.balance < totalAmount) {
             System.out.println("Insufficient balance");
